f289622ec195996a0aab0471e9d66233399fbd79
Graham Collins <grahamcollins70@gmail.com>
2019-08-04 16:49:57 +0100

    negative test and tear down added.

--- Springer/Search.cs
+++ Springer/Search.cs
@@ -23,8 +23,16 @@ namespace Springer
             driver.FindElement(By.Id("search")).Click();
             IWebElement searchresults = driver.FindElement(By.Id("results-list"));
             Assert.IsTrue(searchresults.Text.Contains("Experimental and Computational Multiphase Flow"), "FAIL: Search results do not contain searched for value");
-
-
-                }
+            driver.FindElement(By.Id("logo")).Click();
+            driver.FindElement(By.Id("query")).SendKeys("Xenotaxidermy through the ages");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("search")).Click();
+            String noresults = driver.FindElement(By.Id("no-results-message")).Text;
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Assert.IsTrue(noresults.Contains("Sorry – we couldn’t find what you are looking for"), "FAIL: Search results message not as expected " + noresults);
+            }
+        [TearDown]
+        public void closebrowser()
+        { driver.Close(); }
     }
 }
