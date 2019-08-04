10b997453017b873128a7ad770304ba742037ccf
Graham Collins <grahamcollins70@gmail.com>
2019-08-04 12:36:40 +0100

    Set up - new driver

---
+++ Springer/Search.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+
+namespace Springer
+{
+    class Search
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startbrowser()
+        { driver = new ChromeDriver("C:\\Program Files (x86)\\Google\\Chrome\\Application");
+        }
+    }
+}
